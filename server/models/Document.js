b4c32c478b181eea8a3210f74e5a8c9570a6130e
DevPithadia <devkumarpithadia@gmail.com>
2026-05-30 16:06:38 +0530

    Updated User and Document models

--- server/models/Document.js
+++ server/models/Document.js
@@ -3,7 +3,8 @@ const {Schema, model} = require('mongoose')
 const Document = new Schema({
     _id: String,
     title: { type: String, default: "Untitled Document" },
-    data: Object
+    data: Object,
+    owner: { type: Schema.Types.ObjectId, ref: "User", required: true }
 })
 
 module.exports = model("Document", Document)
