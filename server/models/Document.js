1d9048918286b69984cb549c947daaa5c8dd02a8
DevPithadia <devkumarpithadia@gmail.com>
2026-05-30 16:54:43 +0530

    Document model updated to add owner

--- server/models/Document.js
+++ server/models/Document.js
@@ -1,10 +1,14 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const Document = new Schema({
-    _id: String,
-    title: { type: String, default: "Untitled Document" },
-    data: Object,
-    owner: { type: Schema.Types.ObjectId, ref: "User", required: true }
-})
+  _id: String,
+  title: { type: String, default: "Untitled Document" },
+  data: Object,
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
 
-module.exports = model("Document", Document)
+module.exports = model("Document", Document);
